c59020cf8dbe318b16b91bf9f1eb7725d9988e86
Elliot <ewiltshire@perforce.com>
2015-06-29 15:20:35 -0700

    Re-jigged some of the namespaces to make things work with GitSwarm autoloader - made more compatible with the Mirror class.

--- perforce_swarm/config.rb
+++ perforce_swarm/config.rb
@@ -1,15 +1,11 @@
+require_relative 'init'
 require_relative '../lib/gitlab_config'
 
 module PerforceSwarm
-  module GitFusion
-    module GitlabConfigExtension
-      def git_fusion
-        @config['git_fusion'] ||= {}
-      end
+  class GitlabConfig < GitlabConfig
+    def git_fusion
+      @config['git_fusion'] ||= {}
     end
   end
 end
 
-class GitlabConfig
-  prepend PerforceSwarm::GitFusion::GitlabConfigExtension
-end
--- perforce_swarm/repo.rb
+++ perforce_swarm/repo.rb
@@ -3,40 +3,38 @@ require_relative 'git_fusion'
 require_relative 'utils'
 
 module PerforceSwarm
-  module GitFusion
-    class Repo
-      class << self
-        attr_accessor :error
-      end
+  class Repo
+    class << self
+      attr_accessor :error
+    end
 
-      # returns a hash mapping repo name to description for all repos defined at the configured (or specified)
-      # base URL. returns nil if something went wrong - check the 'error' method if you want details
-      def self.list(url = nil)
-        url    ||= GitlabConfig.new.git_fusion['url']
-        @error   = nil
+    # returns a hash mapping repo name to description for all repos defined at the configured (or specified)
+    # base URL. returns nil if something went wrong - check the 'error' method if you want details
+    def self.list(url = nil)
+      url    ||= PerforceSwarm::GitlabConfig.new.git_fusion['url']
+      @error   = nil
 
-        # run the git fusion @list command
-        output = PerforceSwarm::GitFusion::URL.new(url).clear_path.command('list').run
+      # run the git fusion @list command
+      output = PerforceSwarm::GitFusion::URL.new(url).clear_path.command('list').run
 
-        # parse the Git Fusion repos
-        return parse_repos(output)
-      rescue StandardError => e
-        @error = e.message
-        nil
-      end
+      # parse the Git Fusion repos
+      return parse_repos(output)
+    rescue StandardError => e
+      @error = e.message
+      nil
+    end
 
-      def self.parse_repos(git_output)
-        repos = {}
-        return repos unless git_output
+    def self.parse_repos(git_output)
+      repos = {}
+      return repos unless git_output
 
-        # iterate over each repo found and build a hash mapping repo name to description
-        git_output.lines.each do |repo|
-          if /^(?<name>[\w\-]+)\s+(push|pull)?\s+([\w\-]+)\s+(?<description>.+?)$/ =~ repo
-            repos[name] = description.strip
-          end
+      # iterate over each repo found and build a hash mapping repo name to description
+      git_output.lines.each do |repo|
+        if /^(?<name>[\w\-]+)\s+(push|pull)?\s+([\w\-]+)\s+(?<description>.+?)$/ =~ repo
+          repos[name] = description.strip
         end
-        repos
       end
+      repos
     end
   end
 end
