adc96d78379c15c93eee8b276a6e14d94ef5b7ea
Geoff Nicol <gnicol@perforce.com>
2015-07-10 09:51:46 -0700

    Adding a path accessor to the repo object

--- perforce_swarm/repo.rb
+++ perforce_swarm/repo.rb
@@ -3,10 +3,12 @@ require_relative 'utils'
 
 module PerforceSwarm
   class Repo
-    def initialize(repo_path)
-      repo_path = File.realpath(repo_path)
-      fail 'Not a valid repo path' unless File.exist?(File.join(repo_path, 'config'))
-      @repo_path = repo_path
+    attr_accessor :path
+
+    def initialize(path)
+      path = File.realpath(path)
+      fail 'Not a valid repo path' unless File.exist?(File.join(path, 'config'))
+      @path = path
     end
 
     def mirrored?
@@ -18,11 +20,11 @@ module PerforceSwarm
       # construct the Git Fusion URL based on the mirror URL given
       # run the git command to add the remote
       resolved_url = GitFusionRepo.resolve_url(url)
-      Utils.popen(%w(git remote remove mirror), @repo_path)
-      output, status = Utils.popen(['git', 'remote', 'add', 'mirror', resolved_url], @repo_path)
+      Utils.popen(%w(git remote remove mirror), @path)
+      output, status = Utils.popen(['git', 'remote', 'add', 'mirror', resolved_url], @path)
       @mirror_url    = nil
       unless status.zero? && mirror_url == resolved_url
-        fail "Failed to add mirror remote #{url} to #{@repo_path} its still #{mirror_url}\n#{output}"
+        fail "Failed to add mirror remote #{url} to #{@path} its still #{mirror_url}\n#{output}"
       end
 
       url
@@ -31,7 +33,7 @@ module PerforceSwarm
     def mirror_url
       return @mirror_url if @mirror_url
 
-      @mirror_url, status = Utils.popen(%w(git config --get remote.mirror.url), @repo_path)
+      @mirror_url, status = Utils.popen(%w(git config --get remote.mirror.url), @path)
       @mirror_url.strip!
       @mirror_url = false unless status.zero? && !@mirror_url.empty?
       @mirror_url
