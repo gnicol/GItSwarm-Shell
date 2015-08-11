3743e364b2689a1b98a6855155a652c6a26458fd
Patrycja Tomiak <patrycjatomiak@gmail.com>
2015-08-11 16:19:49 +0100

    Add couple of tests for version checking and result reporting on git_fusion instances

--- perforce_swarm/spec/git_fusion_spec.rb
+++ perforce_swarm/spec/git_fusion_spec.rb
@@ -114,7 +114,7 @@ describe PerforceSwarm::GitFusion do
   exceptions = { 'https://123.23.23.23:443' => 'https://123.23.23.23',
                  'https://localhost:443/repo' => 'https://localhost/repo'
   }
-
+  let(:config) { PerforceSwarm::GitlabConfig.new }
   describe :valid_url? do
     it 'returns true on valid git fusion urls' do
       valid_urls.each do |url|
@@ -496,4 +496,73 @@ describe PerforceSwarm::GitFusion do
       end
     end
   end
+
+  describe :version_check do
+    let(:config) { PerforceSwarm::GitlabConfig.new }
+
+    context 'without global settings' do
+      before do
+        config.instance_variable_set(:@config, YAML.load(<<eos
+git_fusion:
+  enabled: true
+  some_value: some string
+  default:
+    url: "foo@bar"
+  foo:
+    url: "bar@baz"
+  yoda:
+    url: "http://foo@bar"
+eos
+
+                                             )
+        )
+      end
+      it 'returns valid and not outdated if version 2015.2' do
+        git_fusion = PerforceSwarm::GitFusion
+        git_fusion.stub(:run).and_return('Rev. Git Fusion/2015.2/1128995 (2015/06/23)')
+        git_fusion.validate_entries('2015.2').each do | _instance, values |
+          expect(values[:valid]).to be_true
+          expect(values[:version]).to eq('2015.2.1128995')
+          expect(values[:outdated]).to be_false
+        end
+      end
+
+      it 'returns non-valid and outdated if version < 2015.2' do
+        git_fusion = PerforceSwarm::GitFusion
+        git_fusion.stub(:run).and_return('Rev. Git Fusion/2015.1/142456 (2015/05/21)')
+        git_fusion.validate_entries('2015.2').each do | _instance, values |
+          expect(values[:valid]).to be_false
+          expect(values[:version]).to eq('2015.1.142456')
+          expect(values[:outdated]).to be_true
+        end
+      end
+
+      it 'returns non-valid and outdated if we specified patch version' do
+        git_fusion = PerforceSwarm::GitFusion
+        git_fusion.stub(:run).and_return('Rev. Git Fusion/2015.1/121 (2015/05/21)')
+        git_fusion.validate_entries('2015.2.122').each do | _instance, values |
+          expect(values[:valid]).to be_false
+          expect(values[:version]).to eq('2015.1.121')
+          expect(values[:outdated]).to be_true
+        end
+      end
+
+      it 'fails if version specified is not valid' do
+        git_fusion = PerforceSwarm::GitFusion
+        git_fusion.stub(:run).and_return('Rev. Git Fusion/2015.1/121 (2015/05/21)')
+        expect{ git_fusion.validate_entries('A2015/2/122') }
+            .to raise_error(RuntimeError, 'Invalid min_version specified: A2015/2/122')
+      end
+
+      it 'returns error message caught from git command execution' do
+        git_fusion = PerforceSwarm::GitFusion
+        git_fusion.stub(:run).and_raise(PerforceSwarm::GitFusion::RunError, 'Very generic git error.')
+        git_fusion.validate_entries('2015.2').each do | _instance, values |
+          expect(values[:valid]).to be_false
+          expect(values[:error]).to eq('Very generic git error.')
+        end
+      end
+    end
+  end
 end
+
