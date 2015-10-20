53681b300ca5add669374d8ffaaa12a511ea1bcb
Geoff Nicol <gnicol@perforce.com>
2015-10-20 16:28:08 -0700

    Tweaking mirror fetch binary to skip running if we're mid-push.

--- perforce_swarm/mirror.rb
+++ perforce_swarm/mirror.rb
@@ -354,6 +354,23 @@ module PerforceSwarm
       end
     end
 
+    def self.write_locked?(repo_path)
+      # see if we have a mirror remote, if not nothing to do
+      return false unless Repo.new(repo_path).mirrored?
+
+      # the lock is automatically released after the blocks finish, but we manually release the lock for performance.
+      File.open(File.join(repo_path, 'mirror_push.lock'), 'w+', 0644) do |write_handle|
+        begin
+          # we just invert the flock result so we're write_locked? false if we get a lock (as no-one else had one)
+          # and we're write_locked? true if we can't get a lock (as someone else has one)
+          return !write_handle.flock(File::LOCK_EX | File::LOCK_NB)
+        ensure
+          write_handle.flock(File::LOCK_UN)
+          write_handle.close
+        end
+      end
+    end
+
     def self.fetch_locked?(repo_path)
       # see if we have a mirror remote, if not nothing to do
       return false unless Repo.new(repo_path).mirrored?
